8a5b58ae81eb322bdeaa8b3a27aa64242b449377
varshu01 <51811929+varshu01@users.noreply.github.com>
2023-12-14 13:47:07 -0500

    Adding modified index.js

--- recorder-app/src/index.js
+++ recorder-app/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./components/App";
+import { createRoot } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />);
