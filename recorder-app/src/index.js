8b285ea4a5c9b65129dfc6d4288b604e6e67a56c
varshu01 <51811929+varshu01@users.noreply.github.com>
2023-12-14 15:07:51 -0500

    Modifying

--- recorder-app/src/index.js
+++ recorder-app/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import App from "./components/App";
-import { createRoot } from 'react-dom';
 
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
